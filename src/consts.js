fb87cd6bbf231611ab1204a6f5fc961541542477
tqgx <lilyjone1999@hotmail.com>
2023-09-19 20:15:21 +0800

    1

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `TQGX's Site`;
+export const SITE_DESCRIPTION = 'Hello! im Lily Joneś, a.k.a. TQGX OpenTools & Portabler';
+export const SITE_EMAIL = 'lilyjones1999@hotmail.com'
+export const SITE_NAME = 'TQGX';
+export const SITE_URL = "https://tqgx.github.io";
