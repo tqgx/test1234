9c04696426bd51a867ac4a9c7bb26a102fdffb6e
tqgx <lilyjone1999@hotmail.com>
2023-09-19 22:27:44 +0800

    1

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
+    title: "TQGX 's Blog",
     description: "Site description",
     site: 'https://astro-blog.qum.cc',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
